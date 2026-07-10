e7f82d47f8759960fbbeade2ffe7ed603037b4da
Yaseen726 <147266787+Yaseen726@users.noreply.github.com>
2026-07-10 22:22:42 +0530

    setup base settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,21 +1,19 @@
 from pathlib import Path
-
+import environ
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+env = environ.Env()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zikp=ow#h994&tmzud0r3x!bhg%3)4bo)q@a907_f0l%w^#usq'
+SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env.bool('DEBUG',default=False)
 
-DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS',default=[])
 
 # Application definition
 
