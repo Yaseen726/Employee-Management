9a54cf0f85aaf7dad20c15d28b6c892abdaea756
Yaseen726 <147266787+Yaseen726@users.noreply.github.com>
2026-07-11 23:24:12 +0530

    model play

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Department(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    name = models.CharField(
+        max_length=100,
+        unique=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Employee(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    name = models.CharField(max_length=100)
+    department = models.ForeignKey(Department,on_delete=models.PROTECT,related_name="employees")
+
